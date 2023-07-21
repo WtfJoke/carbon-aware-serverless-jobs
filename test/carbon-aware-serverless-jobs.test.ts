4799d5f5a45e5cd67b6d754aebe4fb550a24d3bf
Manuel <manuel.wessner@codecentric.de>
2023-07-21 11:30:46 +0200

    🎉 Ran

---
+++ test/carbon-aware-serverless-jobs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CarbonAwareServerlessJobs from '../lib/carbon-aware-serverless-jobs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/carbon-aware-serverless-jobs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CarbonAwareServerlessJobs.CarbonAwareServerlessJobsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
