4a08e89cd223f6c9c1ce1c35cb1d34fc79dba700
Manuel <manuel.wessner@codecentric.de>
2023-07-28 16:29:43 +0200

    ♻️ Extract CarbonAwareComputing related things into own Construct

--- lib/CarbonAwareServerlessJobsStack.ts
+++ lib/CarbonAwareServerlessJobsStack.ts
@@ -1,14 +1,17 @@
 import * as cdk from "aws-cdk-lib";
-import { Architecture, Runtime, Tracing } from "aws-cdk-lib/aws-lambda";
-import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { StringParameter } from "aws-cdk-lib/aws-ssm";
-import { StateMachine, Wait, WaitTime } from "aws-cdk-lib/aws-stepfunctions";
-import { LambdaInvoke } from "aws-cdk-lib/aws-stepfunctions-tasks";
+import { Pass } from "aws-cdk-lib/aws-stepfunctions";
 import { Construct } from "constructs";
+import { CarbonAwareComputingServerlessJobsConstruct } from "./carbon/carbon-aware-computing/CarbonAwareComputingServleressJobsConstruct";
 
 export class CarbonAwareServerlessJobsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const fakeBatchJobTask = new Pass(this, "My long running batch job", {
+      comment: "This is my long running batch job",
+      resultPath: "$.batchJobOutput",
+    });
 
     const carbonAwareComputingApiKey =
       StringParameter.fromSecureStringParameterAttributes(
@@ -19,51 +22,13 @@ export class CarbonAwareServerlessJobsStack extends cdk.Stack {
         },
       );
 
-    const getBestRenewableEnergyTimeWindowLambda = new NodejsFunction(
+    new CarbonAwareComputingServerlessJobsConstruct(
       this,
-      "GetBestTimeWindow",
+      "CarbonAwareComputing",
       {
-        entry:
-          "lib/carbon/carbon-aware-computing/GetBestRenewableEnergyTimeWindow.ts",
-        handler: "handler",
-        description:
-          "Get the best time window to run a job based on the carbon intensity of the grid using the API of https://www.carbon-aware-computing.com/.",
-        runtime: Runtime.NODEJS_18_X,
-        tracing: Tracing.ACTIVE,
-        memorySize: 512,
-        architecture: Architecture.ARM_64,
-        environment: {
-          CARBON_AWARE_COMPUTING_API_KEY_SECURE_STRING_PARAMETER_NAME:
-            carbonAwareComputingApiKey.parameterName,
-        },
+        apiKey: carbonAwareComputingApiKey,
+        batchJobTask: fakeBatchJobTask,
       },
     );
-
-    carbonAwareComputingApiKey.grantRead(
-      getBestRenewableEnergyTimeWindowLambda,
-    );
-
-    const getStatus = new LambdaInvoke(
-      this,
-      "Get time window for best energy mix",
-      {
-        lambdaFunction: getBestRenewableEnergyTimeWindowLambda,
-        resultPath: "$.bestTimeWindowOutput",
-      },
-    );
-
-    const waitStep = new Wait(this, "Wait for time Window", {
-      time: WaitTime.secondsPath(
-        "$.bestTimeWindowOutput.Payload.waitTimeInSecondsForOptimalExecution",
-      ),
-    });
-
-    const definition = getStatus.next(waitStep);
-
-    new StateMachine(this, "Scheduler", {
-      definition,
-      stateMachineName: "CarbonAwareServerlessCACBatchJobsScheduler",
-      tracingEnabled: true,
-    });
   }
 }
---
+++ lib/carbon/carbon-aware-computing/CarbonAwareComputingServleressJobsConstruct.ts
@@ -0,0 +1,75 @@
+import { IStringParameter } from "aws-cdk-lib/aws-ssm";
+import { Construct } from "constructs";
+import { Runtime, Tracing, Architecture } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import {
+  Wait,
+  WaitTime,
+  DefinitionBody,
+  StateMachine,
+  IChainable,
+} from "aws-cdk-lib/aws-stepfunctions";
+import { LambdaInvoke } from "aws-cdk-lib/aws-stepfunctions-tasks";
+
+interface CarbonAwareComputingServerlessJobsConstructProps {
+  apiKey: IStringParameter;
+  batchJobTask: IChainable;
+}
+
+export class CarbonAwareComputingServerlessJobsConstruct extends Construct {
+  constructor(
+    scope: Construct,
+    id: string,
+    props: CarbonAwareComputingServerlessJobsConstructProps,
+  ) {
+    super(scope, id);
+    const { apiKey, batchJobTask } = props;
+
+    const getBestRenewableEnergyTimeWindowLambda = new NodejsFunction(
+      this,
+      "GetBestTimeWindow",
+      {
+        entry:
+          "lib/carbon/carbon-aware-computing/GetBestRenewableEnergyTimeWindow.ts",
+        handler: "handler",
+        description:
+          "Get the best time window to run a job based on the carbon intensity of the grid using the API of https://www.carbon-aware-computing.com/.",
+        runtime: Runtime.NODEJS_18_X,
+        tracing: Tracing.ACTIVE,
+        memorySize: 512,
+        architecture: Architecture.ARM_64,
+        environment: {
+          CARBON_AWARE_COMPUTING_API_KEY_SECURE_STRING_PARAMETER_NAME:
+            apiKey.parameterName,
+        },
+      },
+    );
+
+    apiKey.grantRead(getBestRenewableEnergyTimeWindowLambda);
+
+    const getStatus = new LambdaInvoke(
+      this,
+      "Get time window for best energy mix",
+      {
+        lambdaFunction: getBestRenewableEnergyTimeWindowLambda,
+        resultPath: "$.bestTimeWindowOutput",
+      },
+    );
+
+    const waitStep = new Wait(this, "Wait for time Window", {
+      time: WaitTime.secondsPath(
+        "$.bestTimeWindowOutput.Payload.waitTimeInSecondsForOptimalExecution",
+      ),
+    });
+
+    const definitionBody = DefinitionBody.fromChainable(
+      getStatus.next(waitStep).next(batchJobTask),
+    );
+
+    new StateMachine(this, "Scheduler", {
+      stateMachineName: "CarbonAwareServerlessCACBatchJobsScheduler",
+      definitionBody,
+      tracingEnabled: true,
+    });
+  }
+}
