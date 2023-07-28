c0fcd25d82a08ad22fcc558a9c4356d5e681ed86
Manuel <manuel.wessner@codecentric.de>
2023-07-28 16:54:31 +0200

    ✅ Add very basic test which makes sure step function is created

--- test/CarbonAwareServerlessJobs.test.ts
+++ test/CarbonAwareServerlessJobs.test.ts
@@ -1,16 +1,12 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CarbonAwareServerlessJobs from '../lib/carbon-aware-serverless-jobs-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { CarbonAwareServerlessJobsStack } from "../lib/CarbonAwareServerlessJobsStack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/carbon-aware-serverless-jobs-stack.ts
-test("SQS Queue Created", () => {
-  //   const app = new cdk.App();
-  //     // WHEN
-  //   const stack = new CarbonAwareServerlessJobs.CarbonAwareServerlessJobsStack(app, 'MyTestStack');
-  //     // THEN
-  //   const template = Template.fromStack(stack);
-  //   template.hasResourceProperties('AWS::SQS::Queue', {
-  //     VisibilityTimeout: 300
-  //   });
+test("step function created", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new CarbonAwareServerlessJobsStack(app, "MyTestStack");
+  // THEN
+  const template = Template.fromStack(stack);
+  template.hasResource("AWS::StepFunctions::StateMachine", {});
 });
