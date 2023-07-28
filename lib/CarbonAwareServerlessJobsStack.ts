bae56eaafd279c783ce23ab4ca3481f49286c55c
Manuel <manuel.wessner@codecentric.de>
2023-07-28 16:40:51 +0200

    🔇 Remove step function execution logging to log group

--- lib/CarbonAwareServerlessJobsStack.ts
+++ lib/CarbonAwareServerlessJobsStack.ts
@@ -1,10 +1,8 @@
 import * as cdk from "aws-cdk-lib";
 import { StringParameter } from "aws-cdk-lib/aws-ssm";
-import { LogLevel, Pass } from "aws-cdk-lib/aws-stepfunctions";
+import { Pass } from "aws-cdk-lib/aws-stepfunctions";
 import { Construct } from "constructs";
 import { CarbonAwareComputingServerlessJobsConstruct } from "./carbon/carbon-aware-computing/CarbonAwareComputingServleressJobsConstruct";
-import { LogGroup, RetentionDays } from "aws-cdk-lib/aws-logs";
-import { RemovalPolicy } from "aws-cdk-lib";
 
 export class CarbonAwareServerlessJobsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -24,26 +22,12 @@ export class CarbonAwareServerlessJobsStack extends cdk.Stack {
         },
       );
 
-    const carbonAwareLogGroup = new LogGroup(
-      this,
-      "CarbonAwareComputingLogGroup",
-      {
-        retention: RetentionDays.ONE_MONTH,
-        removalPolicy: RemovalPolicy.DESTROY,
-      },
-    );
-
     new CarbonAwareComputingServerlessJobsConstruct(
       this,
       "CarbonAwareComputing",
       {
         apiKey: carbonAwareComputingApiKey,
         batchJobTask: fakeBatchJobTask,
-        logOptions: {
-          destination: carbonAwareLogGroup,
-          level: LogLevel.ALL,
-          includeExecutionData: true,
-        },
       },
     );
   }
