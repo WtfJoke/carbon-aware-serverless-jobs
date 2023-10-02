4cabaafbb5c5be495782c78677b31118ccce162c
Manuel <manuel.wessner@codecentric.de>
2023-10-02 17:44:40 +0200

    ♻️ Extract carbon aware computing api calls into own file

--- lib/carbon/carbon-aware-computing/GetBestRenewableEnergyTimeWindow.ts
+++ lib/carbon/carbon-aware-computing/GetBestRenewableEnergyTimeWindow.ts
@@ -1,6 +1,4 @@
 import { Logger } from "@aws-lambda-powertools/logger";
-import { getParameter } from "@aws-lambda-powertools/parameters/ssm";
-import queryString from "query-string";
 
 import {
   CarbonAwareTimeWindowPayload,
@@ -8,22 +6,17 @@ import {
   CarbonAwareTimeWindowResponse,
   CarbonAwareTimeWindowResponseScheme,
 } from "../models";
-import {
-  CarbonAwareComputingForecastQueryParams,
-  CarbonAwareComputingForecastQueryParamsScheme,
-  CarbonAwareComputingForecastResponse,
-} from "./models";
 import dayjs, { Dayjs } from "dayjs";
 import minMax from "dayjs/plugin/minMax";
 import duration from "dayjs/plugin/duration";
 import relativeTime from "dayjs/plugin/relativeTime";
+import { fetchOptimalExecutionStartDate } from "./CarbonAwareComputingClient";
 dayjs.extend(minMax);
 dayjs.extend(duration);
 dayjs.extend(relativeTime);
 
 const serviceName = "carbon-aware-serverless-jobs";
-const logger = new Logger({ serviceName });
-const BASE_API_URL = "https://forecast.carbon-aware-computing.com";
+export const logger = new Logger({ serviceName });
 
 export const handler = async (
   event: CarbonAwareTimeWindowPayload,
@@ -50,11 +43,11 @@ const getCarbonAwareTimeWindow = async (
   const startDate = dayjs(earliestDateTime);
   const latestStartDate = startDate.add(latestStartInMinutes, "minutes");
 
-  const optimalExecutionDateTime = await fetchForecast({
+  const optimalExecutionDateTime: Dayjs = await fetchOptimalExecutionStartDate({
     location,
     dataStartAt: startDate.toISOString(),
     dataEndAt: latestStartDate.toISOString(),
-  }).then(extractOptimalExecutionDate);
+  });
 
   const waitTimeInSecondsForOptimalExecution =
     getWaitTimeInSecondsForOptimalExecution(optimalExecutionDateTime);
@@ -66,47 +59,6 @@ const getCarbonAwareTimeWindow = async (
     });
 
   return response;
-};
-
-const getApiKey = async (): Promise<string> => {
-  const secureStringParameterName = getSecureStringParameterName();
-  const maxAge = 60 * 60 * 24; // One day in seconds
-  const apiKey = await getParameter<string>(secureStringParameterName, {
-    maxAge,
-  });
-
-  if (!apiKey) {
-    throw new Error(
-      `Missing Carbon Aware Computing API key in Parameter ${secureStringParameterName}`,
-    );
-  }
-
-  return apiKey;
-};
-
-const getSecureStringParameterName = (): string => {
-  const secureStringParameterName =
-    process.env.CARBON_AWARE_COMPUTING_API_KEY_SECURE_STRING_PARAMETER_NAME;
-  if (!secureStringParameterName) {
-    throw new Error(
-      "Missing CARBON_AWARE_COMPUTING_API_KEY_SECURE_STRING_PARAMETER_NAME environment variable",
-    );
-  }
-  return secureStringParameterName;
-};
-
-const extractOptimalExecutionDate = async (
-  forecastResponse: CarbonAwareComputingForecastResponse,
-): Promise<Dayjs> => {
-  const forecastOptimalDataPoints = forecastResponse[0].optimalDataPoints;
-  if (!forecastOptimalDataPoints || forecastOptimalDataPoints.length !== 1) {
-    throw new Error(
-      `Expected exactly one optimal data point, got ${forecastOptimalDataPoints?.length}`,
-    );
-  }
-
-  const optimalExecutionDate = forecastOptimalDataPoints[0].timestamp;
-  return dayjs(optimalExecutionDate);
 };
 
 const getWaitTimeInSecondsForOptimalExecution = (
@@ -130,43 +82,4 @@ const getWaitTimeInSecondsForOptimalExecution = (
       .humanize()} for optimal execution`,
   );
   return waitTimeInSecondsForOptimalExecution;
-};
-
-const fetchForecast = async ({
-  location,
-  dataStartAt,
-  dataEndAt,
-}: CarbonAwareComputingForecastQueryParams): Promise<CarbonAwareComputingForecastResponse> => {
-  const apiKey = await getApiKey();
-
-  const queryParams: CarbonAwareComputingForecastQueryParams =
-    CarbonAwareComputingForecastQueryParamsScheme.parse({
-      location,
-      dataStartAt,
-      dataEndAt,
-    });
-
-  const apiEndpoint = `${BASE_API_URL}/emissions/forecasts/current`;
-  const apiUrl = `${apiEndpoint}?${queryString.stringify(queryParams)}`;
-  logger.info("Fetching best time window from apiUrl: " + apiUrl, {
-    apiEndpoint,
-    queryParams,
-    apiUrl,
-  });
-
-  const forecastResponse = await fetch(apiUrl, {
-    headers: {
-      "x-api-key": apiKey,
-    },
-  });
-  if (!forecastResponse.ok) {
-    logger.error(forecastResponse.statusText, {
-      statusCode: forecastResponse.status,
-      errorResponse: await forecastResponse.text(),
-    });
-    throw new Error("Failed to fetch forecast data");
-  }
-  const forecastResponseArray: CarbonAwareComputingForecastResponse =
-    await forecastResponse.json();
-  return forecastResponseArray;
 };
