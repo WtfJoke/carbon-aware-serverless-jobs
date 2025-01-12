84d7fcdc6e07cd76effe6b8d9f0b055d8f24370d
Manuel <wtfjoke@livenet.ch>
2025-01-12 01:40:47 +0100

    ✨ Make it Lambda-less
    The api call can be made using the HTTP Task in step functions (see https://docs.aws.amazon.com/step-functions/latest/dg/call-https-apis.html).
    
    Thanks to JSONata date/time functions (see https://docs.jsonata.org/date-time-functions), I was able to remove the lambda and all needed libraries.
    
    Before JSONata it was not possible to parse/process ISO 8601 timestamps without a lambda. This was needed to calculate timestamps such as `earliestDateTime`, `latestStartInMilliseconds`, `dataEndAt`.

--- lib/carbon/carbon-aware-computing/CarbonAwareComputingServerlessJobsConstruct.ts
+++ lib/carbon/carbon-aware-computing/CarbonAwareComputingServerlessJobsConstruct.ts
@@ -1,19 +1,18 @@
-import { IStringParameter } from "aws-cdk-lib/aws-ssm";
-import { Construct } from "constructs";
-import { Runtime, Tracing, Architecture } from "aws-cdk-lib/aws-lambda";
-import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { SecretValue } from "aws-cdk-lib";
+import { Authorization, Connection } from "aws-cdk-lib/aws-events";
+import { Policy, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { ISecret, Secret } from "aws-cdk-lib/aws-secretsmanager";
 import {
-  Wait,
-  WaitTime,
+  CustomState,
   DefinitionBody,
-  StateMachine,
   IChainable,
   LogOptions,
+  StateMachine,
 } from "aws-cdk-lib/aws-stepfunctions";
-import { LambdaInvoke } from "aws-cdk-lib/aws-stepfunctions-tasks";
+import { Construct } from "constructs";
 
-interface CarbonAwareComputingServerlessJobsConstructProps {
-  apiKey: IStringParameter;
+export interface CarbonAwareComputingServerlessJobsConstructProps {
+  apiKey: ISecret;
   batchJobTask: IChainable;
   logOptions?: LogOptions;
 }
@@ -28,43 +27,72 @@ export class CarbonAwareComputingServerlessJobsConstruct extends Construct {
     super(scope, id);
     const { apiKey, batchJobTask, logOptions } = props;
 
-    const getBestRenewableEnergyTimeWindowLambda = new NodejsFunction(
+    const apiEndpoint =
+      "https://forecast.carbon-aware-computing.com/emissions/forecasts/current";
+
+    const connection = new Connection(this, "CarbonAwareComputingConnection", {
+      authorization: Authorization.apiKey(
+        "x-api-key",
+        SecretValue.secretsManager(apiKey.secretName),
+      ),
+      description: "https://www.carbon-aware-computing.com/",
+    });
+    const connectionSecret = Secret.fromSecretCompleteArn(
       this,
-      "GetBestTimeWindow",
+      "CarbonAwareComputingConnectionSecret",
+      connection.connectionSecretArn,
+    );
+    const earliestDateTimeExpression =
+      "$fromMillis($toMillis($exists($states.input.earliestDateTime) ? $states.input.earliestDateTime : $now()))";
+    const latestStartInMillisecondsExpression =
+      "$exists($states.input.latestStartInMinutes) ? $states.input.latestStartInMinutes * 60 * 1000 : 24 * 60 * 60 * 1000";
+    const fetchBestTimeWindow = new CustomState(
+      this,
+      "Fetch Best Time Window",
       {
-        entry:
-          "lib/carbon/carbon-aware-computing/GetBestRenewableEnergyTimeWindow.ts",
-        handler: "handler",
-        description:
-          "Get the best time window to run a job based on the carbon intensity of the grid using the API of https://www.carbon-aware-computing.com/.",
-        runtime: Runtime.NODEJS_LATEST,
-        tracing: Tracing.ACTIVE,
-        memorySize: 512,
-        architecture: Architecture.ARM_64,
-        environment: {
-          CARBON_AWARE_COMPUTING_API_KEY_SECURE_STRING_PARAMETER_NAME:
-            apiKey.parameterName,
+        stateJson: {
+          QueryLanguage: "JSONata",
+          Type: "Task",
+          Resource: "arn:aws:states:::http:invoke",
+          Arguments: {
+            ApiEndpoint: apiEndpoint,
+            Method: "GET",
+            Authentication: {
+              ConnectionArn: connection.connectionArn,
+            },
+            QueryParameters: {
+              location: "{% $states.input.location %}",
+              dataStartAt: `{% ${earliestDateTimeExpression} %}`,
+              dataEndAt: `{% $fromMillis($toMillis(${earliestDateTimeExpression}) + (${latestStartInMillisecondsExpression})) %}`,
+            },
+          },
+          Retry: [
+            {
+              ErrorEquals: ["States.ALL"],
+              BackoffRate: 2,
+              IntervalSeconds: 1,
+              MaxAttempts: 3,
+              JitterStrategy: "FULL",
+            },
+          ],
+          Assign: {
+            optimalTimeStamp:
+              "{% $states.result.ResponseBody[0].optimalDataPoints[0].timestamp %}",
+          },
         },
       },
     );
 
-    apiKey.grantRead(getBestRenewableEnergyTimeWindowLambda);
-
-    const getStatus = new LambdaInvoke(
-      this,
-      "Get time window for best energy mix",
-      {
-        lambdaFunction: getBestRenewableEnergyTimeWindowLambda,
-        outputPath: "$.Payload",
+    const waitStep = new CustomState(this, "Wait for time Window", {
+      stateJson: {
+        QueryLanguage: "JSONata",
+        Type: "Wait",
+        Timestamp: "{% $optimalTimeStamp %}",
       },
-    );
-
-    const waitStep = new Wait(this, "Wait for time Window", {
-      time: WaitTime.secondsPath("$.waitTimeInSecondsForOptimalExecution"),
     });
 
     const definitionBody = DefinitionBody.fromChainable(
-      getStatus.next(waitStep).next(batchJobTask),
+      fetchBestTimeWindow.next(waitStep).next(batchJobTask),
     );
 
     this.stateMachine = new StateMachine(this, "Scheduler", {
@@ -73,5 +101,29 @@ export class CarbonAwareComputingServerlessJobsConstruct extends Construct {
       logs: logOptions,
       tracingEnabled: true,
     });
+
+    connectionSecret.grantRead(this.stateMachine);
+    this.stateMachine.role.attachInlinePolicy(
+      new Policy(this, "InvokeHttpEndpointPolicy", {
+        statements: [
+          new PolicyStatement({
+            sid: "AllowInvokeHttpEndpoint",
+            actions: ["states:InvokeHTTPEndpoint"],
+            resources: [this.stateMachine.stateMachineArn],
+            conditions: {
+              StringEquals: {
+                "states:HTTPEndpoint": [apiEndpoint],
+                "states:HTTPMethod": ["GET"],
+              },
+            },
+          }),
+          new PolicyStatement({
+            sid: "RetrieveConnectionCredentials",
+            actions: ["events:RetrieveConnectionCredentials"],
+            resources: [connection.connectionArn],
+          }),
+        ],
+      }),
+    );
   }
 }
